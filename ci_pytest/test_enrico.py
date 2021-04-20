fb5ef147cf1722f1a01aa7dcbd17cdc62682494f
Ron Rahaman <rahaman@mcs.anl.gov>
2021-04-20 12:51:00 -0500

    WIP: pytest parameterization from xml works

--- ci_pytest/test_enrico.py
+++ ci_pytest/test_enrico.py
@@ -1,73 +1,66 @@
 import pytest
+import os
+from xml.etree import ElementTree
+from collections import namedtuple
+
+CaseParam = namedtuple('CaseParam',
+                       ['neutronics_driver', 'heat_fluids_driver', 'casename', 'casedir',
+                        'cmake_opts'])
+
+
+def pytest_generate_tests(metafunc):
+    # For each case, get its name, directory, and CMake options
+    root = ElementTree.parse("ci_pytest/pytest_config.xml").getroot()
+    argvalues = []
+    for drivers in root.findall("drivers"):
+        neutronics_driver = drivers.get("neutronics")
+        heat_fluids_driver = drivers.get("heat_fluids")
+        for case in drivers.findall("case"):
+            casename = case.get("name")
+            casedir = case.get("dir")
+            cmake_opts = ""
+            if drivers.get("heat_fluids_driver") == "nek5000":
+                cmake_opts = f"{cmake_opts} -DNEK_DIST=nek5000 -DUSR_LOC={casedir}"
+            elif drivers.get("heat_fluids_driver") == "nekrs":
+                cmake_opts = f"{cmake_opts} -DNEK_DIST=nekrs"
+            else:
+                cmake_opts = f"{cmake_opts} -DNEK_DIST=none"
+            argvalues.append(CaseParam(
+                neutronics_driver=neutronics_driver,
+                heat_fluids_driver=heat_fluids_driver,
+                casename=casename,
+                casedir=casedir,
+                cmake_opts=cmake_opts))
+
+    metafunc.parametrize("case_param", argvalues)
+    metafunc.parametrize("make", argvalues, indirect=True)
+    metafunc.parametrize("cmake", argvalues, indirect=True)
 
 
 @pytest.fixture(scope="module")
 def download_xs():
     print("<<< download xs >>>")
 
+
 @pytest.fixture(scope="module")
 def unzip_nek_statefile():
     print("<<< unzip nek statefile >>>")
-
 
 @pytest.fixture(scope="module")
 def patch_nek_input():
     print("<<< unzip nek input >>>")
 
 
-class TestOpenmcSurrogate:
-    @pytest.fixture(scope="class", autouse=True)
-    def get_input_data(self, download_xs):
-        pass
-
-    @pytest.fixture(scope="class")
-    def configure(self):
-        print("<<< configure OpenMC + surrogate >>>")
-
-    @pytest.fixture(scope="class", autouse=True)
-    def compile(self, configure):
-        print("<<< compiling OpenMC + surrogate >>>")
-
-    def test_shortrod(self):
-        print("<<< testing OpenMC + surrogate shortrod >>>")
-        assert True
-
-    def test_longrod(self):
-        print("<<< testing OpenMC + surrogate longrod >>>")
-        assert True
+@pytest.fixture(scope="function")
+def cmake(request):
+    cmake_cmd = f"From fixture CMAKE: {request.param}"
+    print(cmake_cmd)
 
 
-class TestOpenmcNek5000:
-    @pytest.fixture
-    def nek_case_info(self):
-        print("<<< ran nek_case_info >>>")
-        return {}
+@pytest.fixture(scope="function")
+def make(request, cmake):
+    print(f"From fixture MAKE: {request.param}")
 
-    @pytest.fixture
-    def compile(self, nek_case_info):
-        # Assert that nek_case_info has some usable values?
-        print(f"<<< in compile, got nek_case_info = {nek_case_info} >>>")
 
-    @pytest.fixture
-    def run(self, nek_case_info, compile):
-        print(f"<<< in run, got nek_case_info = {nek_case_info} >>>")
-
-    @pytest.fixture
-    def use_short_case(self, nek_case_info):
-        nek_case_info["casedir"] = "tests/singlerod/short/nek5000"
-        nek_case_info["casename"] = "rodcht"
-        print("<<< ran use_short_case >>>")
-        return nek_case_info
-
-    @pytest.fixture
-    def use_long_case(self, nek_case_info):
-        nek_case_info["casedir"] = "tests/singlerod/long/nek5000"
-        nek_case_info["casename"] = "rod_l"
-        print("<<< ran use_long_case >>>")
-        return nek_case_info
-
-    def test_short(self, use_short_case, run):
-        assert True
-
-    def test_long(self, use_long_case, run):
-        assert True
+def test_run(case_param, make):
+    print(f"From func test_run: {case_param}")
